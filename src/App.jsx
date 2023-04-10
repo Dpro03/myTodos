2967e543d8931b197b172f49340f75ba2249dd6e
David Proto <davidproto@sbcglobal.net>
2023-04-09 21:58:12 -0400

    Todos are persistent

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,88 @@
-import "./styles.css"
+import { useState, useEffect } from "react";
+import "./styles.css";
+
 export default function App() {
+  const [newItem, setNewItem] = useState("");
+  const [todos, setTodos] = useState(() => {
+    const localValue = localStorage.getItem("ITEM");
+    if (localValue == null) return [];
+    return JSON.parse(localValue);
+  });
+
+
+  useEffect(() => {
+    localStorage.setItem("ITEM", JSON.stringify(todos))
+  }, [todos])
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    // console.log(newItem);
+
+    setTodos((currentTodos) => {
+      return [
+        ...currentTodos,
+        { id: crypto.randomUUID(), title: newItem, completed: false },
+      ];
+    });
+
+    setNewItem("");
+  }
+
+  function toggleTodo(id, completed) {
+    setTodos((currentTodos) => {
+      return currentTodos.map((todo) => {
+        if (todo.id === id) {
+          return { ...todo, completed };
+        }
+        return todo;
+      });
+    });
+  }
+
+  function deleteTodo(id) {
+    setTodos((currentTodos) => {
+      return currentTodos.filter((todo) => todo.id !== id);
+    });
+  }
+
   return (
     <>
-  <form className='new-item-form'>
-   <div className='form-row'>
-    <label htmlFor='item'>New Item</label>
-    <input type='text' id='item' />
-   </div>
-   <button className='btn'>Add</button>
-  </form>
-  <h1 className='header'>Todo List</h1>
-  <ul className='list'>
-    <li>
-      <label>
-        <input type='checkbox' />
-        Item 1
-      </label>
-      <button className="btn btn-danger">Delete</button>
-    </li>
-    <li>
-      <label>
-        <input type='checkbox' />
-        Item 2
-      </label>
-      <button className="btn btn-danger">Delete</button>
-    </li>
-
-    </ul>
-  </>
-  )
+      <form onSubmit={handleSubmit} className="new-item-form">
+        <div className="form-row">
+          <label htmlFor="item">New Item</label>
+          <input
+            value={newItem}
+            onChange={(e) => setNewItem(e.target.value)}
+            type="text"
+            id="item"
+          />
+        </div>
+        <button className="btn">Add</button>
+      </form>
+      <h1 className="header">Todo List</h1>
+      <ul className="list">
+        {todos.length === 0 && <li>No 🛠️ Todos</li>}
+        {todos.map((todo) => {
+          return (
+            <li key={todo.id}>
+              <label>
+                <input
+                  type="checkbox"
+                  checked={todo.completed}
+                  onChange={(e) => toggleTodo(todo.id, e.target.checked)}
+                />
+                {todo.title}
+              </label>
+              <button
+                onClick={() => deleteTodo(todo.id)}
+                className="btn btn-danger"
+              >
+                Delete
+              </button>
+            </li>
+          );
+        })}
+      </ul>
+    </>
+  );
 }
